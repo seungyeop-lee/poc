a9fc03efc9dabd28c13b13346d19d9f2b0cbbaa4
seungyeop-lee <xbeast7@gmail.com>
2023-11-23 19:24:44 +0900

    twophase-login: 프로젝트 기본 구성

---
+++ java/spring-security/twophase-login/login-back/src/test/java/com/example/loginback/LoginBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/LoginFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginFrontApplication.class, args);
+    }
+
+}
