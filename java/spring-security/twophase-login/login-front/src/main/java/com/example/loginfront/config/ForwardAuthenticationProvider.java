44578c961ead9c959a8f59b307e7abd23f65f19c
seungyeop-lee <xbeast7@gmail.com>
2023-11-25 15:38:54 +0900

    twophase-login: 프론트 로그인을 ajax를 이용하게 변경

--- java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/config/ForwardAuthenticationProvider.java
+++ java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/config/ForwardAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package com.example.loginfront.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -7,9 +8,11 @@ import org.springframework.security.core.AuthenticationException;
 
 import java.util.List;
 
+@Slf4j
 public class ForwardAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        log.info("authentication: {}", authentication);
         return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), null, List.of());
     }
 
