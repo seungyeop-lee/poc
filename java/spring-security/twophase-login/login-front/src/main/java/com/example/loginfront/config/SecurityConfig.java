dc8788ce1dd01c6115857059341766301203b853
seungyeop-lee <xbeast7@gmail.com>
2023-11-26 12:53:59 +0900

    twophase-login: 세션 관련 설정 추가

--- java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/config/SecurityConfig.java
+++ java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/config/SecurityConfig.java
@@ -31,6 +31,18 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/")
         );
 
+        http.sessionManagement(c -> c
+                .sessionFixation(sf -> sf
+                        .changeSessionId() //로그인 시 동일한 세션에 대한 id 변경
+                )
+                // authentication의 principle의 값이 중복되는지 확인해서 session 관리를 하는 것으로 보임
+                .sessionConcurrency(sc -> sc
+                        .maximumSessions(1) //사용자 당 최대 몇 session까지 허용하는지 설정
+                        .maxSessionsPreventsLogin(false) //true면 로그인 금지, false면 로그인 가능 그러나 그 전 session은 만료
+                        .expiredUrl("/member/login") //session 만료되었을 때 redirect url
+                )
+        );
+
         return http.build();
     }
 }
--- java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/service/LoginForwardService.java
+++ java/spring-security/twophase-login/login-front/src/main/java/com/example/loginfront/service/LoginForwardService.java
@@ -2,8 +2,6 @@ package com.example.loginfront.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -31,17 +29,19 @@ public class LoginForwardService {
         return result.get("uuid");
     }
 
-    public boolean confirmLogin(String uuid) {
+    public String confirmLogin(String uuid) {
         log.debug("uuid: {}", uuid);
-        ResponseEntity<Void> response = restClient.post()
+        Map<String, String> result = restClient.post()
                 .uri("/login/confirm")
                 .contentType(APPLICATION_JSON)
                 .body(Map.of("uuid", uuid))
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {})
-                .toBodilessEntity();
-        log.debug("response: {}", response);
-        return response.getStatusCode().is2xxSuccessful();
+                .body(Map.class);
+        log.debug("response: {}", result);
+        if (result.get("approval").equals("true")) {
+            return result.get("id");
+        } else {
+            return null;
+        }
     }
-
 }
