16650f680a68a2498f3d5128403bab324b1affb5
seungyeop-lee <xbeast7@gmail.com>
2025-10-10 16:23:54 +0900

    serverless/hono-vercel-supabase 초기화

--- serverless/hono-vercel-supabase/server-app/src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
-
-export const testTable = pgTable('test_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  createdAt: timestamp('created_at').defaultNow()
-})
