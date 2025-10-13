d13450ea18380b46b7fb619e41b97a90d26ae285
seungyeop-lee <xbeast7@gmail.com>
2025-10-13 13:38:23 +0900

    serverless/hono-vercel-supabase: mTLS 서버 기본 추가

--- serverless/hono-vercel-supabase/mtls-server/src/index.ts
+++ serverless/hono-vercel-supabase/mtls-server/src/index.ts
@@ -1,15 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import {serve} from '@hono/node-server'
+import {Hono} from 'hono'
+import {createServer} from "node:https";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!!!')
 })
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  createServer: createServer,
+  serverOptions: {
+    key: Buffer.from(process.env.SERVER_KEY!, 'base64').toString('utf-8'),
+    cert: Buffer.from(process.env.SERVER_CERT!, 'base64').toString('utf-8'),
+    ca: Buffer.from(process.env.CA_CERT!, 'base64').toString('utf-8'),
+    requestCert: true,
+    rejectUnauthorized: true,
+  },
+  port: 3001
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  console.log(`Server is running on https://localhost:${info.port}`)
 })
